6f4733132e024e8e80da6718107a990714fd65c4
Arthur <arthur01raphael@gmail.com>
2025-12-18 22:54:19 -0300

    resolvi nalgo que estáva me incomodando

--- nivel1Alura/orientação-a-objetos/screenSound/musica.cs
+++ nivel1Alura/orientação-a-objetos/screenSound/musica.cs
@@ -1,7 +1,7 @@
 ﻿class Musica
 {
-    public string nome;
-    public string artista;
+    public string nome = "";
+    public string artista = "";
     public int duracao;
     public bool disponivel;
 } 
