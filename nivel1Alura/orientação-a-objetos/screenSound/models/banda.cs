dc0e93c7ee41135a2a58e6b69d184e38117fd9db
Arthur <arthur01raphael@gmail.com>
2025-12-24 16:40:59 -0300

    integrando as classes no screenSound 3.0 ,adaptando o projeto para construir um ambiente limpo e trinando a orientação a objetos e classes

--- nivel1Alura/orientação-a-objetos/screenSound/models/banda.cs
+++ nivel1Alura/orientação-a-objetos/screenSound/models/banda.cs
@@ -3,12 +3,15 @@ namespace ScreenSound.models;
 class Banda
 {
     private List<Album> albuns = new List<Album>();
+    private List <int> notas = new();
 
     public Banda(string nome)
     {
         Nome =  nome;
     }
     public string Nome { get; }
+    public double MediaDeNotas => 
+    notas.Average();
 
     public void AdicionarAlbum(Album album)
     {
@@ -19,6 +22,11 @@ class Banda
     {
         Console.WriteLine("Discografia do álbum:\n");
     albuns.ForEach(album => Console.WriteLine(@$"Álbum: {album.Nome} ({album.DuracaoTotal})"));
+    }
+
+    public void AdicionarNota(int nota)
+    {
+        notas.Add(nota);
     }
 
 }
